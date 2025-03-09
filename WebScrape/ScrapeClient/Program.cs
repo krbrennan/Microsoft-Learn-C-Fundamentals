dcfd4ed2ac769cee7dcb03a788d0359b63778891
Kevin Brennan <krb.brennan@gmail.com>
2025-03-08 22:07:11 -0500

    Gitkraken is spazzing out and not adding tracked files

---
+++ WebScrape/ScrapeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using ScrapeLibrary;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebPage("https://learnxinyminutes.com/csharp/");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebScrape/ScrapeLibrary/ScrapeLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+
+        }
+    }
+}
